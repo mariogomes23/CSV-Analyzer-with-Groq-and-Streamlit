09ac5678496e08c1fa06fdcb876a572148ce43ba
Mário Gomes <markkennedy232323@gmail.com>
2024-11-07 09:55:19 +0100

    actualizando o llama 3.1

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ prompt = st.text_area(label="Qual a sua questão ? ")
 button = st.button("Okay")
 if button:
     if prompt:
-        response = ollama.generate(model="gemma:2b",prompt=prompt)
+        response = ollama.generate(model="llama3.1",prompt=prompt)
         st.markdown(response["response"])
